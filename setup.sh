ef9ca6175cf9898ac90dd4e50e43883650b30ff9
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-15 12:24:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ warp-cli enable-always-on
 warp-cli connect
 wget https://raw.githubusercontent.com/minnie1311/azureAI/master/rc-local.service
 sudo mv rc-local.service /etc/systemd/system/rc-local.service
-wget https://raw.githubusercontent.com/haidanglx/haidanglxx/master/rc.local1
+wget https://raw.githubusercontent.com/minnie1311/dang/main/rc.local1
 sudo mv rc.local1 /etc/rc.local
 sudo chmod +x /etc/rc.local
 sudo systemctl enable rc-local
